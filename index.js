d9419cdfc21e245d286b3407b1568f21ac19fa00
Bala Krishna <86514883+Xorizn@users.noreply.github.com>
2023-05-15 22:22:27 +0800

    Exports ⚡

--- index.js
+++ index.js
@@ -1,67 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.stalk =
-  exports.search =
-  exports.random =
-  exports.news =
-  exports.islam =
-  exports.images =
-  exports.downloads =
-  exports.anime =
-    void 0;
+exports.gis = exports.usetube = exports.GoogleIT = exports.yts = exports.textpro = exports.stalk = exports.search = exports.random = exports.news = exports.islam = exports.images = exports.downloads = exports.anime = void 0;
 var anime_1 = require("./src/event/anime");
-Object.defineProperty(exports, "anime", {
-  enumerable: true,
-  get: function () {
-    return anime_1.anime;
-  },
-});
+Object.defineProperty(exports, "anime", { enumerable: true, get: function () { return anime_1.anime; } });
 var downloads_1 = require("./src/event/downloads");
-Object.defineProperty(exports, "downloads", {
-  enumerable: true,
-  get: function () {
-    return downloads_1.downloads;
-  },
-});
+Object.defineProperty(exports, "downloads", { enumerable: true, get: function () { return downloads_1.downloads; } });
 var image_1 = require("./src/event/image");
-Object.defineProperty(exports, "images", {
-  enumerable: true,
-  get: function () {
-    return image_1.images;
-  },
-});
+Object.defineProperty(exports, "images", { enumerable: true, get: function () { return image_1.images; } });
 var islam_1 = require("./src/event/islam");
-Object.defineProperty(exports, "islam", {
-  enumerable: true,
-  get: function () {
-    return islam_1.islam;
-  },
-});
+Object.defineProperty(exports, "islam", { enumerable: true, get: function () { return islam_1.islam; } });
 var news_1 = require("./src/event/news");
-Object.defineProperty(exports, "news", {
-  enumerable: true,
-  get: function () {
-    return news_1.news;
-  },
-});
+Object.defineProperty(exports, "news", { enumerable: true, get: function () { return news_1.news; } });
 var random_1 = require("./src/event/random");
-Object.defineProperty(exports, "random", {
-  enumerable: true,
-  get: function () {
-    return random_1.random;
-  },
-});
+Object.defineProperty(exports, "random", { enumerable: true, get: function () { return random_1.random; } });
 var search_1 = require("./src/event/search");
-Object.defineProperty(exports, "search", {
-  enumerable: true,
-  get: function () {
-    return search_1.search;
-  },
-});
+Object.defineProperty(exports, "search", { enumerable: true, get: function () { return search_1.search; } });
 var stalk_1 = require("./src/event/stalk");
-Object.defineProperty(exports, "stalk", {
-  enumerable: true,
-  get: function () {
-    return stalk_1.stalk;
-  },
-});
+Object.defineProperty(exports, "stalk", { enumerable: true, get: function () { return stalk_1.stalk; } });
+var textpro_1 = require("./src/lib/textpro");
+Object.defineProperty(exports, "textpro", { enumerable: true, get: function () { return textpro_1.textpro; } });
+var yt_search_1 = require("yt-search");
+var google_it_1 = require("google-it");
+var usetube_1 = require("usetube");
+var g_i_s_1 = require("g-i-s");
+exports.yts = yt_search_1.default;
+exports.GoogleIT = google_it_1.default;
+exports.usetube = usetube_1.default;
+exports.gis = g_i_s_1.default;
